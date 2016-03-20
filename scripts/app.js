8522ccbf080bf7536b87410c1fc9665088e974bf
Armando Musto <armando.musto@gmail.com>
2016-03-20 14:56:17 -0400

    Initial steps to expand mygithub home page
    Cleanup of main and details section
    Added ui-router

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,42 @@
-angular.module('app', ['ui.bootstrap', 'ngAnimate'])
+angular.module('app', ['ui.bootstrap', 'ui.router', 'ngAnimate'])
+    .run(
+    [          '$rootScope', '$state', '$stateParams',
+        function ($rootScope,   $state,   $stateParams) {
+
+            // It's very handy to add references to $state and $stateParams to the $rootScope
+            // so that you can access them from any scope within your applications.For example,
+            // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+            // to active whenever 'contacts.list' or one of its decendents is active.
+            $rootScope.$state = $state;
+            $rootScope.$stateParams = $stateParams;
+        }
+    ]
+)
+    .config(function($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('home', {
+                url: "/",
+                templateUrl: "index.html",
+                views: {
+                    "userDetails": {
+                        templateUrl: 'views/common/viewUserDetails.html'
+                    }/*,
+                    "mainView": {
+                        templateUrl: 'faq/faq.html',
+                        controller: 'faqController'
+                    }*/
+                }
+            })
+
+
+
+
+    })
     .controller('mainController', function($scope) {
         $scope.programmingLanguages = {
             angular : true,
