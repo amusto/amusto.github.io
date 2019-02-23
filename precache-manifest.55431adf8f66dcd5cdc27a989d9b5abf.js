3f58db65e388973b1f864f9309fc53207076f793
Armando Musto <armando.musto@gmail.com>
2019-02-22 21:27:31 -0500

    Updated with react build

---
+++ precache-manifest.55431adf8f66dcd5cdc27a989d9b5abf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa11eb060447a2e42efe",
+    "url": "/amusto/amusto.github.io/static/js/runtime~main.fa11eb06.js"
+  },
+  {
+    "revision": "42102c978d904d78b7df",
+    "url": "/amusto/amusto.github.io/static/js/main.42102c97.chunk.js"
+  },
+  {
+    "revision": "dba44bbd85110a2f514b",
+    "url": "/amusto/amusto.github.io/static/js/2.dba44bbd.chunk.js"
+  },
+  {
+    "revision": "42102c978d904d78b7df",
+    "url": "/amusto/amusto.github.io/static/css/main.f8cca986.chunk.css"
+  },
+  {
+    "revision": "a8f956ac0544c3ff9b5cb31a1c61cf5d",
+    "url": "/amusto/amusto.github.io/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/amusto/amusto.github.io/precache-manifest.55431adf8f66dcd5cdc27a989d9b5abf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/amusto/amusto.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
