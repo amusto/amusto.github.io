d1491c77d053f2afab7b1c7e6a08029aead08b87
Armando Musto <armando.musto@gmail.com>
2019-02-22 21:09:24 -0500

    update with react

---
+++ build/precache-manifest.8aaa2d7e23b6b0fed7cc2b01c9b588b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d45f78ce610d30630ee2",
+    "url": "/static/js/main.d45f78ce.chunk.js"
+  },
+  {
+    "revision": "cd96e4c32159259d6c20",
+    "url": "/static/js/2.cd96e4c3.chunk.js"
+  },
+  {
+    "revision": "d45f78ce610d30630ee2",
+    "url": "/static/css/main.f8cca986.chunk.css"
+  },
+  {
+    "revision": "49e6269faaf554d325d1fe23d1316175",
+    "url": "/index.html"
+  }
+];
